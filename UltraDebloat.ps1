cc56ad2c4be73b6913d699180137c6b2d3b99095
jszczerbinsky <chickenmanpl@protonmail.com>
2021-11-21 08:42:15 +0100

    Confirmation to run script

--- UltraDebloat.ps1
+++ UltraDebloat.ps1
@@ -1,34 +1,52 @@
-. ".\config.ps1"
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Run script"
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Don't run script"
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 
-cd actions
-. ".\loadDefaultUser.ps1"
+$result = $host.ui.PromptForChoice("", "
+	This script will make changes on Your system. Make sure it is properly configured and 
+	run it only if You know, what You are doing. Do You want to run it?`n`n", 
+	$options, 1)
+	
+switch ($result)
+{
+  0{
+    . ".\config.ps1"
 
-if($removeApps)
-{
-	Write-Host "Removing apps"
-	. ".\removeApps.ps1"
-}
-if($disableFeatures)
-{
-	Write-Host "Removing features"
-	. ".\disableFeatures.ps1"
-}
-if($protectPrivacy)
-{
-	Write-Host "Protecting privacy"
-	. ".\protectPrivacy.ps1"
-}
-if($banHosts)
-{
-	Write-Host "Banning hosts"
-	. ".\banHosts.ps1"
-}
-if($disableServices)
-{
-	Write-Host "Disabling services"
-	. ".\disableServices.ps1"
-}
+	cd actions
+	. ".\loadDefaultUser.ps1"
 
-. ".\unloadDefaultUser.ps1"
-Write-Host "Done"
-cd ..
+	if($removeApps)
+	{
+		Write-Host "Removing apps"
+		. ".\removeApps.ps1"
+	}
+	if($disableFeatures)
+	{
+		Write-Host "Removing features"
+		. ".\disableFeatures.ps1"
+	}
+	if($protectPrivacy)
+	{
+		Write-Host "Protecting privacy"
+		. ".\protectPrivacy.ps1"
+	}
+	if($banHosts)
+	{
+		Write-Host "Banning hosts"
+		. ".\banHosts.ps1"
+	}
+	if($disableServices)
+	{
+		Write-Host "Disabling services"
+		. ".\disableServices.ps1"
+	}
+
+	. ".\unloadDefaultUser.ps1"
+	Write-Host "Done"
+	cd ..
+  }
+  1
+  {
+    return
+  }
+}
